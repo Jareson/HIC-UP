756e61b6374383d359564d508858808548529965
Jareson Vardisec <jaresonv2011@gmail.com>
2018-08-02 02:20:19 -0500

    Setup XAML Base and made the UI for the landing page.

---
+++ HICUP/HICUP/HICUP/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HICUP
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        void OnShoppingMode()
+        {
+            shoppingMode.IsEnabled = false;
+            shoppingMode.Text = "Shopping Mode";
+        }
+
+        void OnViewInventory()
+        {
+            viewInventory.IsEnabled = false;
+            viewInventory.Text = "View Inventory";
+        }
+
+        void OnModInventory()
+        {
+            modifyInventory.IsEnabled = false;
+            modifyInventory.Text = "Modify Inventory";
+        }
+
+        void OnMakeGroceryList()
+        {
+            makeGroceryList.IsEnabled = false;
+            makeGroceryList.Text = "Make Grocery List";
+        }
+    }
+}

7cf1a2171a84a940de5e0289b04d07dd19196728
Jareson Vardisec <jaresonv2011@gmail.com>
2018-08-16 23:54:41 -0500

    Added Shopping Mode UI Elements

---
+++ HICUP/HICUP/HICUP/Views/RemoveItem.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HICUP.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RemoveItem : ContentPage
+	{
+		public RemoveItem()
+		{
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();
+		}
+
+    }
+}
--- HICUP/HICUP/HICUP/Views/ShoppingMode.xaml.cs
+++ HICUP/HICUP/HICUP/Views/ShoppingMode.xaml.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin.Essentials;
+using HICUP.Views;
 
 namespace HICUP
 {
@@ -18,5 +19,20 @@ namespace HICUP
             NavigationPage.SetHasNavigationBar(this, false);
             InitializeComponent();     
         }
-	}
+
+        async void OnAddItem()
+        {
+            await Navigation.PushAsync(new AddItem());
+        }
+
+        async void OnRemoveItem()
+        {
+            await Navigation.PushAsync(new RemoveItem());
+        }
+
+        async void OnModifyItem()
+        {
+            await Navigation.PushAsync(new ModifyItem());
+        }
+    }
 }
